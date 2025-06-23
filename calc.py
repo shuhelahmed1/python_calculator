28011050a896558bce1d198a8dce8e5a2132101d
Shuhel Ahmed <shuhel037@gmail.com>
2025-06-23 03:32:43 -0400

    first commit

---
+++ calc.py
@@ -0,0 +1,17 @@
+num1 = int(input('First number '))
+sign = input('')
+num2 = int(input('Second number '))
+
+if sign == '+':
+    print(num1+num2)
+elif sign == '-':
+    print(num1-num2)
+elif sign =='*':
+    print(num1*num2)
+elif sign == '/':
+    print(num1/num2)
+    
+
+
+
+
